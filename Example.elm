752c8384f6b0c4640b64373e0347d0cedd5d0e9b
Dandandan <danielheres@gmail.com>
2014-06-24 07:55:10 +0200

    add experimental "expect" support

--- Example.elm
+++ Example.elm
@@ -7,16 +7,17 @@ import String
 
 data Date = Date Int Int Int
 p = integer <* symbol '/'
-parses = 
-    [ parse ((symbol 2) <|> (symbol 1)) [1] |> show
-    , parseString digit "1" |> show
-    , parseString (satisfy isDigit) "1" |> show
-    , parseString (token (String.toList "123")) "123" |> show
-    , parseString (many (symbol '1') <* end) "111" |> show
-    , parseString integer "-100" |> show
-    , parseString float "1.2154" |> show
-    , parseString (Date `map` p `and` p `and` integer) "11/12/2012" |> show
+parses =
+    [ parse ((symbol 2) <|> (symbol 1)) [1] |> show |> plainText
+    , parseString digit "1" |> show |> plainText
+    , parseString (satisfy isDigit) "1" |> show |> plainText
+    , parseString (token (String.toList "123")) "123" |> show |> plainText
+    , parseString (many (symbol '1') <* end) "111" |> show |> plainText
+    , parseString integer "-100" |> show |> plainText
+    , parseString float "1.2154" |> show |> plainText 
+    , parseString (Date `map` p `and` p `and` integer) "11/12/2012" |> show |> plainText
+    , parseString (symbol 'x' <* end) "x1231x" |> show |> plainText
     ]
 
 
-main = asText parses
+main = flow down parses
--- Parser.elm
+++ Parser.elm
@@ -9,7 +9,7 @@ module Parser where
 @docs map, or, and
 
 #Combinators
-@docs succeed, satisfy, empty, symbol, token, choice, optional, many, some, seperatedBy, end
+@docs succeed, satisfy, empty, expect, symbol, token, choice, optional, many, some, seperatedBy, end
 
 #Core functions (infix operators)
 @docs (<*>), (<$>), (<|>), (<*), (*>), (<$)
@@ -19,14 +19,21 @@ import String
 import Either (..)
 import List
 
-type Parser a r = [a] -> [(r, [a])]
+data Result a r
+  = Succeed [(r, [a])]
+  | Expect String [a]
+  | Fail                
+
+type Parser a r = [a] -> Result a r
 
 {-| Parse a list using a parser -}
 parse : Parser a r -> [a] -> Either String r
 parse p xs =
   case p xs of
-    ((e, _)::_) -> Right e
-    _           -> Left "Parse Error"
+    Succeed ((e, _)::_) -> Right e
+    Expect  e (x::_)    -> Left ("unexpected " ++ show x ++ ", expected " ++ e)
+    Expect  e []        -> Left ("error: " ++ e)
+    _                   -> Left "parse error"
 
 {-| Parse a `String` using a `Char` parser  -}
 parseString : Parser Char r -> String -> Either String r
@@ -34,29 +41,29 @@ parseString p = parse p . String.toList
 
 {-| Parser that always succeeds without consuming input -}
 succeed : r -> Parser a r
-succeed b xs = [(b,xs)]
+succeed b xs = Succeed [(b, xs)]
 
 {-| Parser that satisfies a given predicate -}
 satisfy : (a -> Bool) -> Parser a a
 satisfy p xs = 
   case xs of
-    [] -> []
-    (x::xs') -> if p x then [(x, xs')] else []
+    [] -> Fail
+    (x::xs') -> if p x then Succeed [(x, xs')] else Fail
 
 {-| Parser that always fails -}
 empty : Parser a r
-empty = always []
+empty = always <| Fail
 
 {-| Parses a symbol -}
 symbol : a -> Parser a a
-symbol = satisfy . (==)
+symbol x = satisfy (\s -> s == x) `expect` (show x)
 
 {-| Parses a token of symbols -}
 token : [a] -> Parser a [a]
 token xs     =
-    case xs of
-        []      -> succeed []
-        (x::xs) -> (::) `map` symbol x `and` token xs
+  case xs of
+    []      -> succeed []
+    (x::xs) -> (::) `map` symbol x `and` token xs
 
 {-| Combine a list of parsers -}
 choice : [Parser a r] -> Parser a r
@@ -66,12 +73,20 @@ choice = foldr or empty
 optional : Parser a r -> r -> Parser a r
 optional p x = p `or` succeed x
 
+{-| Adds a message in case parsing fails
+
+      aOrB = symbol 'a' `or` symbol 'b' `expect` "a or b"
+
+-}
+expect : Parser a r -> String -> Parser a r
+expect p s = p `or` Expect s
+
 {-| Parses zero or more occurences of a parser -}
 many : Parser a r -> Parser a [r]
 many p xs = --(::) <$> p <*> many p <|> succeed [] (lazy version)
     case p xs of
-        [] -> succeed [] xs
-        _ -> ((::) `map` p `and` many p) xs
+        Succeed _ -> ((::) `map` p `and` many p) xs
+        _         -> succeed [] xs
 
 {-| Parses one or more occurences of a parser -}
 some : Parser a r -> Parser a [r]
@@ -79,29 +94,64 @@ some p = (::) `map` p `and` many p
 
 {-| Map a function over the result of the parser 
 
-      count = length `map` (many digit)
+      count = length `map` many digit
 
 -}
 map : (r -> s) -> Parser a r -> Parser a s
-map f p = List.map (\(r,ys) -> (f r, ys)) . p
+map f p = listToR . List.map (\(r,ys) -> (f r, ys)) . rToList . p
+
+rToList r =
+  case r of
+    Succeed s -> s
+    _         -> []
+
+listToR xs =
+  case xs of
+    [] -> Fail
+    _  -> Succeed xs
 
 {-| Choice between two parsers
 
       oneOrTwo = symbol '1' `or` symbol '2'
 -}
 or : Parser a r -> Parser a r -> Parser a r
-or p q xs = p xs ++ q xs
+or p q xs = 
+  case (p xs, q xs) of
+    (Succeed a, Succeed b)   -> Succeed (a ++ b)
+    (Succeed a, _)           -> Succeed a
+    (_, Succeed b)           -> Succeed b
+    (Expect a _, Expect b _) -> Expect (a ++ " or " ++ b) xs
+    (Expect a _ , _)         -> Expect a xs
+    (_, Expect b _)          -> Expect b xs
+    _                        -> Fail
+
+
+isExpect r = case r of
+    Expect _ _ -> True
+    _          -> False
 
 {-| Sequence two parsers 
 
     data Date = Date Int Int Int
-    map Date year `and` month `and` day
+    date = Date `map` year `and` month `and` day
 -}
 and : Parser a (r -> s) -> Parser a r -> Parser a s
 and p q xs =
-    let a = p xs
-        b = concat <| List.map (\(_,ys) -> q ys) a
-    in zipWith (\(f, ys) (b, zs) -> (f b, zs)) a b
+  let a = p xs
+      a' = rToList a
+      b = List.map (\(_,ys) -> q ys) a'
+      b' = concat <| List.map rToList b
+  in
+    case (zipWith (\(f, ys) (b, zs) -> (f b, zs)) a' b') of
+      [] -> case a of
+          Expect e xs        -> Expect e xs
+          Succeed ((r,a)::_) -> 
+            case filter isExpect b of
+              Expect e es :: _  -> Expect e es
+              _                 -> Fail
+          _                     -> Fail
+      x  -> listToR x
+
 {-| Choice between two parsers -}
 (<|>) : Parser a r -> Parser a r -> Parser a r
 (<|>) = or
@@ -126,7 +176,7 @@ p <* q = always `map` p `and` q
 
 {-| Variant of `<*>` that ignores the result of the parser at the left -}
 (*>) : Parser a s -> Parser a r -> Parser a r
-p *> q = (flip always) `map` p `and` q
+p *> q = flip always `map` p `and` q
 
 {-| Parses a sequence of the first parser, seperated by the second parser -} 
 seperatedBy : Parser a r -> Parser a s -> Parser a [r]
@@ -136,12 +186,13 @@ seperatedBy p s = (::) `map` p `and` many (s *> p)
 end : Parser a ()
 end xs = case xs of
     [] -> succeed () xs
-    _  -> []
+    _  -> Fail
 
 infixl 4 <*>
 infixl 4 `and`
 infixr 3 <|>
 infixr 3 `or`
+infixr 3 `expect`
 infixl 4 <$>
 infixl 4 `map`
 infixl 4 <$
